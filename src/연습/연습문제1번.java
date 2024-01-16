b938c9e168c3bde369f76fa9384845be22abfde7
KangSungLee <rkdtjd1020@naver.com>
2024-01-16 17:31:19 +0900

    제모

---
+++ src/연습/연습문제1번.java
@@ -0,0 +1,20 @@
+package 연습;
+
+public class 연습문제1번 {
+
+	public static void main(String[] args) {
+	
+	int score = 72;
+	if(score >= 80 && score < 90) {
+		System.out.println("B학점");
+	} else if (score >= 90) {
+		System.out.println("A학점");
+	} else if (score >= 70 && score < 80) {
+		System.out.println("C학점");
+	} else {
+		System.out.println("F학점");
+	}
+	
+	}
+
+}
---
+++ src/연습/연습문제3번.java
@@ -0,0 +1,16 @@
+package 연습;
+
+public class 연습문제3번 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; ; i++) {
+			if (i % 2 - 1 == 0) {
+				continue;
+			}
+			if (i > 10)
+				break;
+			System.out.println(i);
+		}
+	}
+}
