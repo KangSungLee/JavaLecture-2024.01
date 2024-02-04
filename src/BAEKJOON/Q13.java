019dc832028ea410ca3166ce0d039e42bda26dc1
kangsunglee <rkdtjd1020@naver.com>
2024-02-04 21:13:50 +0900

    백준

--- src/BAEKJOON/Q13.java
+++ src/BAEKJOON/Q13.java
@@ -1,9 +1,18 @@
+package BAEKJOON;
+
+import java.util.Scanner;
 
 public class Q13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt(), b = scan.nextInt();
+		if (a < b) {
+			System.out.println("<");
+		} else if(a > b) {
+			System.out.println(">");
+		} else {
+			System.out.println("==");
+		}
 	}
-
 }
